8654141ecf040615af021d7fb24681b56d5b1cd6
a-petr <arm.petros@gmail.com>
2023-04-07 23:51:47 -0400

    added the new class

--- base.py
+++ base.py
@@ -60,6 +60,59 @@ class OrthogonallyWeightedL21:
         return self
 
 
+class OrthogonallyWeightedL21Continued:
+    def __init__(
+            self,
+            alpha=False,
+            normalize=False,
+            max_iter=1000,
+            noise_level=1,
+            tol=1e-4,
+            warm_start=False,
+            verbose=False,
+            gamma_tol=False
+    ):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.normalize = normalize
+        self.tol = tol
+        self.warm_start = warm_start
+        self.noise_level = noise_level
+        self.gamma_tol = gamma_tol
+        self.verbose = verbose
+        self.coef_ = None
+
+    def fit(self, A, Y):
+        if self.normalize:
+            A_mean = A.mean(axis=0)
+            A_std = A.std(axis=0)
+            A_std[A_std == 0] = A_mean[A_std == 0]
+            Y_mean = Y.mean(axis=0)
+
+            A -= A_mean[None, :]
+            A /= A_std[None, :]
+            Y -= Y_mean[None, :]
+
+        n_samples, n_features = A.shape
+        n_targets = Y.shape[1]
+
+        if not self.warm_start:
+            self.coef_ = np.zeros((n_features, n_targets), dtype=A.dtype.type)
+
+        self.coef_ = ut.reweighted_coordinate_descent_multi_task_continued(
+            self.coef_,
+            A,
+            Y,
+            self.max_iter,
+            self.noise_level,
+            self.tol,
+            self.alpha,
+            self.verbose,
+            self.gamma_tol)
+
+        return self
+
+
 if __name__ == "__main__":
     owl = OrthogonallyWeightedL21(alpha=0.1,
                                   noise_level=0.0001,
